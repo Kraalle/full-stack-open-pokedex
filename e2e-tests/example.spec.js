4dd4fc337964b9ac5f7b98f0b1e2263795ed428f
Max McLaren <maxtova@yahoo.com>
2024-12-01 10:45:43 -0700

    e2e tests init

---
+++ e2e-tests/example.spec.js
@@ -0,0 +1,20 @@
+// @ts-check
+const { test, describe, expect, beforeEach } = require("@playwright/test");
+
+describe("Pokedex", () => {
+  test("front page can be opened", async ({ page }) => {
+    await page.goto("http://localhost:8080/");
+    await expect(page.getByText("ivysaur")).toBeVisible();
+    await expect(
+      page.getByText(
+        "Pokémon and Pokémon character names are trademarks of Nintendo."
+      )
+    ).toBeVisible();
+  });
+
+  test("navigates to a specific Pokemon page", async ({ page }) => {
+    await page.goto("http://localhost:8080/");
+    await page.click("text=ivysaur");
+    await expect(page.getByText("chlorophyll")).toBeVisible();
+  });
+});
