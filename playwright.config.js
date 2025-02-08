6fce409df98bf4dfb0f6bc1e95fe150b1c769712
Max McLaren <maxtova@yahoo.com>
2025-02-08 08:16:09 -0700

    changed port

--- playwright.config.js
+++ playwright.config.js
@@ -72,7 +72,7 @@ module.exports = defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run start:ci",
-    port: 8080,
+    port: 3000,
     timeout: 10000,
   },
 });
