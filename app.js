a7711d6514a20bf0bbe81ed0f2dc6c8781aee36f
Max McLaren <maxtova@yahoo.com>
2025-06-14 13:03:30 -0700

    works fine

--- app.js
+++ app.js
@@ -6,12 +6,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
-// app.get('/health', (req, res) => {
-//   res.send('ok')
-// })
-
 app.get('/health', (req, res) => {
-  res.status(500).send('error')
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
